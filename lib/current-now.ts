b726ce9f776e7583bdd09f2d9ce59a0c49c9df5a
Ivan <i.pabayo3000@gmail.com>
2026-03-09 19:20:35 +0800

    ADDED function for default DateTime

---
+++ lib/current-now.ts
@@ -0,0 +1,12 @@
+export function currentNow(): string {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const date = String(now.getDate()).padStart(2, "0");
+  const hour = String(now.getHours()).padStart(2, "0");
+  const minute = String(now.getMinutes()).padStart(2, "0");
+  const second = String(now.getSeconds()).padStart(2, "0");
+  const milliseconds = now.getMilliseconds();
+
+  return `${year}-${month}-${date}T${hour}:${minute}:${second}.${milliseconds}Z`;
+}
